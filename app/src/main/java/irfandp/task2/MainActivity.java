53b93d84825aaf9756a3515615d65f9ff59d1391
irfand19 <irfand19@gmail.com>
2016-09-13 15:20:09 +0700

    Intent beres, design per halaman belum fix

--- app/src/main/java/irfandp/task2/MainActivity.java
+++ app/src/main/java/irfandp/task2/MainActivity.java
@@ -1,5 +1,6 @@
 package irfandp.task2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,15 +23,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,18 +72,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_home) {
+            Intent hal_home = new Intent(getApplicationContext(), Home.class);
+            startActivityForResult(hal_home,0);
+        } else if (id == R.id.nav_profile) {
+            Intent hal_profile = new Intent(getApplicationContext(), Profile.class);
+            startActivityForResult(hal_profile,0);
+        } else if (id == R.id.nav_contact) {
+            Intent hal_contact = new Intent(getApplicationContext(), Contact.class);
+            startActivityForResult(hal_contact,0);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
